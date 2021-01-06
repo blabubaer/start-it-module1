0ae12b2c1ee749edd5ac812bb83b887154e86d78
blabubaer <mgransow@gmail.com>
2021-01-06 13:41:31 +0100

    reordered

---
+++ app.js
@@ -0,0 +1,5 @@
+const content = document.getElementsByTagName("link")[1].import
+const snowman = content.getElementsByTagName("svg")
+const contentdiv = document.getElementById("content")
+
+contentdiv.innerHTML = content
